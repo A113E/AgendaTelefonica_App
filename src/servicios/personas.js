5ff3b33588ac347ca4ce55d625cd68cd435916ba
A113E <amg11amg2@gmail.com>
2025-10-18 20:18:48 -0400

    Produccion

--- src/servicios/personas.js
+++ src/servicios/personas.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/api/personas'
+const baseUrl = 'api/personas'
 
 // Servicio para obtener las personas
 function obtenerPersonas() {
